49f156d3907fbb73aeaf06636af0984f01e2c44f
Fred neusgat <your@email.example>
2024-08-17 16:36:17 +0200

    addition of unit test

--- cmake-build-debug/CMakeFiles/complex_datastructures.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for complex_datastructures.
-# This may be replaced when dependencies are built.
---
+++ tests/trie_tests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Maurice on 16/08/2024.
+//
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+
+int main(){
+
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
